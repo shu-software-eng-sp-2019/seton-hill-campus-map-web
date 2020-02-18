702ae4c95702b1e541ff3490c323e115831b0163
ckifer <ck108860@yahoo.com>
2020-02-18 10:40:09 -0800

    Updates

---
+++ precache-manifest.a973343933d6b490e0bd5ac0004d1e9a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693eabbd150873200d68d25e8a033511",
+    "url": "/seton-hill-campus-map-web/index.html"
+  },
+  {
+    "revision": "552dc2adb384cb1d6bce",
+    "url": "/seton-hill-campus-map-web/static/css/3.550a2f40.chunk.css"
+  },
+  {
+    "revision": "41798a620f06a764f02b",
+    "url": "/seton-hill-campus-map-web/static/css/main.7961fe13.chunk.css"
+  },
+  {
+    "revision": "bcbb69a6df498e9f856c",
+    "url": "/seton-hill-campus-map-web/static/js/0.021be879.chunk.js"
+  },
+  {
+    "revision": "552dc2adb384cb1d6bce",
+    "url": "/seton-hill-campus-map-web/static/js/3.fb26a947.chunk.js"
+  },
+  {
+    "revision": "97f9604cfe86d654385dadd41885f363",
+    "url": "/seton-hill-campus-map-web/static/js/3.fb26a947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468eb83ae90d6b8c928f",
+    "url": "/seton-hill-campus-map-web/static/js/4.4c436176.chunk.js"
+  },
+  {
+    "revision": "c1119620902e45bc4f94",
+    "url": "/seton-hill-campus-map-web/static/js/5.bdddffd2.chunk.js"
+  },
+  {
+    "revision": "6dfec5b84506e35ee3ca",
+    "url": "/seton-hill-campus-map-web/static/js/6.55d7b9a2.chunk.js"
+  },
+  {
+    "revision": "e289d6fd47a905fbfa26",
+    "url": "/seton-hill-campus-map-web/static/js/7.41b8683b.chunk.js"
+  },
+  {
+    "revision": "0173de7be4061fe4c736",
+    "url": "/seton-hill-campus-map-web/static/js/8.0604ccfc.chunk.js"
+  },
+  {
+    "revision": "41798a620f06a764f02b",
+    "url": "/seton-hill-campus-map-web/static/js/main.1df67c40.chunk.js"
+  },
+  {
+    "revision": "6687726e711c50c26792",
+    "url": "/seton-hill-campus-map-web/static/js/runtime-main.8b0147f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seton-hill-campus-map-web/precache-manifest.211a1f8054dc25f786f71948931edaa1.js"
+  "/seton-hill-campus-map-web/precache-manifest.a973343933d6b490e0bd5ac0004d1e9a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/seton-hill-campus-map-web/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/seton-hill-campus-map-web/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
